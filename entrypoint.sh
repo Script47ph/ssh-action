b51d89b8793e2cae81b6a7ff68f616ac394a3289
Script47 <54595497+Script47ph@users.noreply.github.com>
2024-04-09 11:14:39 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -63,4 +63,4 @@ TARGET="${GITHUB_ACTION_PATH}/${CLIENT_BINARY}"
 echo "Will download ${CLIENT_BINARY} from ${DOWNLOAD_URL_PREFIX}"
 curl -fL --retry 3 --keepalive-time 2 "${DOWNLOAD_URL_PREFIX}/${CLIENT_BINARY}" -o ${TARGET}
 chmod +x ${TARGET}
-sh -c "${TARGET} $*" | printf "outtest=%s\n" "$(cat)" >> $GITHUB_OUTPUT
+sh -c "${TARGET} $*"
