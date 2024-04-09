9c1712f6b3ca0cee7462b495920320f334ab916b
Script47 <54595497+Script47ph@users.noreply.github.com>
2024-04-09 11:35:25 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -85,3 +85,5 @@ fi
 # stderr=$(cat /tmp/errFile)
 # echo "stdout=${stdout//$'\n'/\\n}" >> $GITHUB_OUTPUT
 # echo "stderr=${stderr//$'\n'/\\n}" >> $GITHUB_OUTPUT
+printf "out=%s\n" "$(cat /tmp/outFile)" >> $GITHUB_OUTPUT
+printf "err=%s\n" "$(cat /tmp/errFile)" >> $GITHUB_OUTPUT
